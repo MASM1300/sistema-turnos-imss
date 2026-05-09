8ab25dbf8b009d0582fd03a4f6eb76fd8f3ae74f
MASM1300 <smerari3@gmail.com>
2026-05-08 19:25:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,10 @@ def handle_turno(data):
     emit('actualizar_pantalla', data, broadcast=True)
 
 # 5. Arrancar el servidor
+import os
+
 if __name__ == '__main__':
-    # Importante: No uses app.run, usa socketio.run
-    socketio.run(app, host='0.0.0.0', port=5000)
+    # Render nos asigna un puerto dinámico, hay que leerlo:
+    port = int(os.environ.get('PORT', 5000))
+    # Usamos socketio.run directamente
+    socketio.run(app, host='0.0.0.0', port=port)
